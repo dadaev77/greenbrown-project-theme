7945fb0591d87856631fb8ba58a0693195a784e0
Aslan <jasmina031015@gmail.com>
2024-10-26 01:27:40 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <h1>Welcome to my website</h1>
+    <link href="{{ asset('css/themes/classic/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
